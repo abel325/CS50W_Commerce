2fac4f33bc27a96a7c41fc0243c9755920bb216a
Abel Mehedinteanu <me.abel52@gmail.com>
2023-11-21 15:40:26 +0200

    Change models layout + migrate

--- auctions/models.py
+++ auctions/models.py
@@ -5,14 +5,25 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class AuctionListings(models.Model):
+class AuctionListing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    title = models.CharField(max_length=128)
+    description = models.TextField(null=True)
+    image = models.BinaryField(null=True) # upload_to parameter may be needed
+    category = models.ForeignKey('AuctionCategory', on_delete=models.SET_NULL, null=True, related_name="listings")
+    starting_bid = models.FloatField()
+    current_bid = models.FloatField()
+
+
+class Bid(models.Model):
+    value = models.FloatField()
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     pass
 
-class Bids(models.Model):
+class Comment(models.Model):
     pass
 
-class Comments(models.Model):
-    pass
-
-class AuctionCategories(models.Model):
+class AuctionCategory(models.Model):
+    name = models.CharField(max_length=64)
     pass
