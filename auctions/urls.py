58c7d4bdfff93e56acd5d853ab47a460bce247be
Abel Mehedinteanu <me.abel52@gmail.com>
2023-11-23 22:47:48 +0200

    add urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("new_listing", views.new_listing, name="new_listing"),
+    path("listing_page/<int:listing_id>", views.listing_page, name='listing_page'),
+    path('add_to_watchlist/<int:listing_id>', views.add_to_watchlist, name='add_to_watchlist'),
+    path('remove_from_watchlist/<int:listing_id>', views.remove_from_watchlist, name='remove_from_watchlist'),
+    path('bid/<int:listing_id>', views.bid, name='bid'),
+    path('close_listing/<int:listing_id>', views.close_listing, name='close_listing'),
+    path('my_listings', views.my_listings, name='my_listings'),
+    path('watchlist', views.watchlist, name='watchlist'),
+    path('won_listings', views.won_listings, name='won_listings'),
 ]
